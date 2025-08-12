6af72f6c491075a37559d873766abf142d1cff50
devopsodia <odiadevops@gmail.com>
2025-08-13 01:23:29 +0530

    Updated Resource Tab data view

--- backend/src/routes/resourcesRoutes.js
+++ backend/src/routes/resourcesRoutes.js
@@ -1,7 +1,7 @@
 // routes/resourcesRoutes.js
 import express from 'express';
 import { listResources } from '../controllers/resourcesController.js';
-import { rbac } from '../middlewares/rbacMiddleware.js';
+import { rbac } from '../middleware/rbacMiddleware.js';
 
 const router = express.Router();
 
