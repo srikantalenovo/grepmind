ee62b09a1ea4716ee64c716efdd2cb54a1cc21ae
devopsodia <odiadevops@gmail.com>
2025-08-15 01:42:17 +0530

    updated node clickable and kubelet logs mixed

--- backend/src/routes/clusterRoutes.js
+++ backend/src/routes/clusterRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/namespaces', rbac(['viewer', 'editor', 'admin']), getNamespaces);
 router.get('/nodes', rbac(['viewer', 'editor', 'admin']), getNodes);
 router.get('/nodes/:name/details',  rbac(['viewer', 'editor', 'admin']), getNodeDetails);
-router.get('/cluster/nodes/:name/logs',  rbac(['viewer', 'editor', 'admin']), getNodeLogs);
-router.get('/cluster/nodes/:name/events', rbac(['viewer', 'editor', 'admin']), getNodeEvents);
+router.get('/nodes/:name/logs',  rbac(['viewer', 'editor', 'admin']), getNodeLogs);
+router.get('/nodes/:name/events', rbac(['viewer', 'editor', 'admin']), getNodeEvents);
 
 export default router;
